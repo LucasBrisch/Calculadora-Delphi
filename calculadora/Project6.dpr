2f0161004a2ce76199fbdea157eb3cf9e1281744
Lucas Brisch <lucasbrisch@gmail.com>
2025-07-14 23:39:46 -0300

    initial commit

---
+++ calculadora/Project6.dpr
@@ -0,0 +1,14 @@
+program Project6;
+
+uses
+  Vcl.Forms,
+  calculadora in 'calculadora.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
